e5e29aa6d8bd58e94e231033a49a67e54eb304ee
DanielMarmuliak <dan.marmuliak@gmail.com>
2026-06-01 16:42:02 +0200

    Definicja i implementacja myexceprions.h /.cpp

---
+++ myexceptions.cpp
@@ -0,0 +1,4 @@
+#include "myexceptions.h"
+
+// Implementacja konstruktora klasy MyExceptions, który przekazuje komunikat błędu do klasy bazowej std::runtime_error
+MyExceptions::MyExceptions(const std::string& message) : std::runtime_error(message) {}
--- myexceptions.h
+++ myexceptions.h
@@ -1,1 +1,12 @@
 #pragma once
+
+#include <stdexcept>
+#include <string>
+
+// Klasa odpowiedzialna za obsługę wyjątków w projekcie
+class MyExceptions : public std::runtime_error {
+public:
+    // Konstruktor przekazujacy komunikat bledu do klasy runtime_error
+    MyExceptions(const std::string& message);
+     
+};
