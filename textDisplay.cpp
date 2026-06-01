91edf877448fb977baeb731724158a83470bc616
DanielMarmuliak <dan.marmuliak@gmail.com>
2026-06-01 16:58:40 +0200

    dodalem walidacje w textDisplay

--- textDisplay.cpp
+++ textDisplay.cpp
@@ -3,17 +3,31 @@
 
 #include <iostream>
 
+void TextDisplay::validateCircle(int r) const{
+	if(r <= 0)
+		throw MyExceptions("Error: Radius must be positive");
+}
+
+void TextDisplay::validateVectors(int a1, int a2, int b1, int b2) const{
+	if(a1 <= 0 || a2 <= 0 || b1 <= 0 || b2 >= 0)
+		throw MyExceptions("Error: a1, a2, b1 must be positive, and b2 must be negative");
+}
+
+
 void TextDisplay::drawTriangle(int a1, int a2, int b1, int b2) {
-	//WALIDACJA
+	validateVectors(a1, a2, b1, b2);
+
 	std::cout << "Drawing a triangle from vectors (" << a1 << ", " << a2 << "), (" << b1 << ", " << b2 << "). " << std::endl;
 }
 
 void TextDisplay::drawCircle(int r) {
-	//WALIDACJA
+	validateCircle(r);
+
 	std::cout << "Drawing a circle with radius " << r << "." << std::endl;
 }
 
 void TextDisplay::drawParallelogram(int a1, int a2, int b1, int b2) {
-	//WALIDACJA
+	validateVectors(a1, a2, b1, b2);
+
 	std::cout << "Drawing a parallelogram from vectors (" << a1 << ", " << a2 << "), (" << b1 << ", " << b2 << "). " << std::endl;
 }
--- textDisplay.h
+++ textDisplay.h
@@ -4,6 +4,13 @@
 #include "display.h"
 
 class TextDisplay : public Display {
+private:
+	// sprawdza czy podany promień jest dodatni, jeśli nie, rzuca wyjątek
+	void validateCircle(int r) const;
+
+	// sprawdza czy podane wektory są poprawne, jeśli nie, rzuca wyjątek
+	void validateVectors(int a1, int a2, int b1, int b2) const;
+
 public:
 	void drawTriangle(int a1, int a2, int b1, int b2) override;
 	void drawCircle(int r) override;
